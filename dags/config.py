016ba7c26788935c37c06cab498e48cc7c7547a8
Valentin Bauer <valentin.bauer@hotmail.fr>
2024-12-10 12:48:03 +0000

    break everything

--- dags/config.py
+++ dags/config.py
@@ -1,4 +1,5 @@
 import os
+import datetime as datetime
 
 PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,4 +14,8 @@ GENERATED_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.c
 FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 
 MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
-MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, 'model.joblib')  # To change when needed
+MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, 'model.joblib')  # Default value
+
+def get_model_path(model_name):
+    model_name_with_tstp = concat(str(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"), '_model.joblib'))
+    return os.path.join(MODEL_REGISTRY_FOLDER, model_name, '.joblib')  # Default value
