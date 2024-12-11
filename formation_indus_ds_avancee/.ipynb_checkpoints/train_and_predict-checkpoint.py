87358be5f513aabf692012ab9c6aa81e760e940e
Valentin Bauer <valentin.bauer@hotmail.fr>
2024-12-11 08:41:11 +0000

    added mlflow autologs

--- formation_indus_ds_avancee/.ipynb_checkpoints/train_and_predict-checkpoint.py
+++ formation_indus_ds_avancee/.ipynb_checkpoints/train_and_predict-checkpoint.py
@@ -18,8 +18,8 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     X = features.drop(columns=[target])
     y = features[target]
     with mlflow.start_run():
-        mlflow.sklearn.autolog()
-        model = RandomForestRegressor(n_estimators=1, max_depth=15, n_jobs=1)
+        # insert autolog here ...
+        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
@@ -39,6 +39,3 @@ def predict(features: pd.DataFrame, model_path: str) -> pd.DataFrame:
     model = joblib.load(model_path)
     features['predictions'] = model.predict(features)
     return features
-
-
-
